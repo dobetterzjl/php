7fbc3e64a380a62316e6b501c2a483f0ccfb3948
elegant1225 <1737289219@qq.com>
2016-12-27 23:49:33 +0800

    del

---
+++ del.php
@@ -0,0 +1,13 @@
+<?php
+	include "conn.php";
+	if(isset($_GET['id'])){
+		$id=$_GET['id'];
+	}
+	$sql = "delete from blog where blogid='$id'";
+	$query = mysqli_query($link,$sql);
+	if($query){
+		echo "<script>alert('删除成功')</script>";
+		echo "<script>location='index.php'</script>";
+	}
+
+?>
