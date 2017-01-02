24e1e56f5596d0f3afae493b1d0c676bdc6b70a9
elegant1225 <1737289219@qq.com>
2017-01-02 23:11:25 +0800

    添加了更新删除操作

--- ci1exersics/application/controllers/blog.php
+++ ci1exersics/application/controllers/blog.php
@@ -11,5 +11,36 @@
             $arr['blog']=$rs;
             $this->load->view('index.php',$arr);
         }
+        public function update(){
+            $id=$this->uri->segment(3);
+            $this->load->model('blog_model');
+            $result=$this->blog_model->up_attr($id);
+            $arr['up']=$result;
+            $this->load->view('update.php',$arr);
+        }
+        public function upact(){
+            $hid =$this->input->post('hid');
+            $title=$this->input->post('title');
+            $content = $this->input->post('con');
+            $this->load->model('blog_model');
+            $result=$this->blog_model->update_act($hid,$title,$content);
+           if($result){
+               redirect('blog/index');
+
+           }
+        }
+        public function del(){
+            $id = $this->uri->segment(3);
+            //echo $id;
+            $this->load->model('blog_model');
+            $result=$this->blog_model->del_attr($id);
+            if($result){
+                //$this->index();
+                redirect('blog/index');
+            }else{
+                echo "删除失败";
+            }
+            
+        }
     }
 ?>
