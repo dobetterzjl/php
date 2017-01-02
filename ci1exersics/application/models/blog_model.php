aff455ab714e874793b9bc22da2d5f6bd62cf582
elegant1225 <1737289219@qq.com>
2017-01-02 23:09:44 +0800

    添加了更新删除操作

--- ci1exersics/application/models/blog_model.php
+++ ci1exersics/application/models/blog_model.php
@@ -5,5 +5,19 @@
             $result=$query->result();
             return $result;
         }
+        public function up_attr($id){
+            $query=$this->db->get_where('blog',array('blogid'=>$id));
+            return $query->row();
+        }
+        public function update_act($hid,$title,$content){
+            $d=Date('Y').'-'.Date('m').'-'.Date('d');
+            $this->db->where('blogid',$hid);
+           $query= $this->db->update('blog',array('title'=>$title,'content'=>$content,'time'=>$d));
+            return $query;
+        }
+        public function del_attr($id){
+            $query=$this->db->delete('blog',array('blogid'=>$id));
+            return $query;
+        }
     }
 ?>
